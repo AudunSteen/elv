e8f8baf7af5c2ff00db02f15f5d5d2de441be380
AudunSteen <audunste@uia.no>
2022-02-01 18:20:09 +0100

    3

--- state/state.go
+++ state/state.go
@@ -2,17 +2,8 @@ package state
 
 func ViewState() string {
 	// Sjekke data som er lagret ("kylling til venstre", "rev til venstre")
-	return "[kylling rev korn hs ---\\_________\\/ _______________/---]"
+	return "[kylling rev korn hs ---\\ \\__/ _________________/---]"
 }
-func ViewState() string {
-	// kylling rev korn og mann hopper i båten")
-	return "[V----------\\kylling rev korn hs\\/ ----------//---Ø]"
-}
-func ViewState() string {
-	// kylling rev korn og mann går opp på østsiden av elven")
-	return "[V----------\\___________\\/ kylling rev korn hs//---Ø]"
-}
-3
 
 func PutInBoat() string {
 	return "rev"
